cf41ade1b1cd6052525dca1736279a7d4f1eb00c
0lafe <0lafethehorsescrub@gmail.com>
2022-03-11 19:46:18 -0600

    finished base backend

--- app/serializers/recipe_type_by_item_output_serializer.rb
+++ app/serializers/recipe_type_by_item_output_serializer.rb
@@ -2,7 +2,7 @@ class RecipeTypeByItemOutputSerializer < ActiveModel::Serializer
   attributes :id, :name, :gui_url, :scale, :recipe_quantity
 
   def recipe_quantity
-    Recipe.left_joins(:recipe_type).where(recipe_type: object).left_joins(:outputs).where(outputs: { item_id: scope }).count
+    Recipe.left_joins(:recipe_type).where(recipe_type: object).left_joins(:outputs).where(outputs: { item_id: scope }).uniq.count
   end
     
 end
